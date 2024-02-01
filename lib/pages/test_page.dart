26019431c258413946e45bbd01e99532270e0f41
Theo22100 <theoguerinBZH@hotmail.com>
2024-02-01 12:13:13 +0100

    Oubli test_page

--- lib/pages/test_page.dart
+++ lib/pages/test_page.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:my_app/modules/http.dart';
-import 'package:my_app/pages/login_page.dart';
-import 'package:my_app/pages/register_page.dart';
+import 'package:my_app/pages/register-login/login_page.dart';
+import 'package:my_app/pages/register-login/register_page.dart';
 
 // Classe représentant la page principale
 class TestPage extends StatefulWidget {
