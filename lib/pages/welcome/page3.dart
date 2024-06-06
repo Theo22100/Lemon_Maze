0da32afa9d7352cf4e5f187738c47280865c34a8
Theo22100 <theoguerinBZH@hotmail.com>
2024-06-06 11:55:56 +0200

    Commentaire var non utilisé

--- lib/pages/welcome/page3.dart
+++ lib/pages/welcome/page3.dart
@@ -6,7 +6,7 @@ class Welcome3Page extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final double screenWidth = MediaQuery.of(context).size.width;
+    //final double screenWidth = MediaQuery.of(context).size.width;
     final double screenHeight = MediaQuery.of(context).size.height;
     return Scaffold(
       body: Container(
